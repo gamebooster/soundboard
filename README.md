77ca1d7e9408e02ec7bec3b8dfcbe1a551dbbceb
Karl Skomski <mail@skomski.com>
2020-05-28 16:14:13 +0200

    windows: Initial commit. base func usable

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # soundboard
 
+### works
+
+* on windows with sound and microphone playing on the same time 
+
+### not working
+
+* gui
+* config
+* code mess
+* linux
+* mac
+
+### current usage
+
+1. ` soundboard --print-possible-devices`
+2. `soundboard -i <index> -o <index>`
+3. `Press CTRL-P to play nicht-so-tief-ruediger.mp3`
+5. `???`
+4. `Press CTRL-C to exit`
+
+#### example
+
+````
+PS K:\projects\soundboard> .\target\debug\soundboard.exe --help
+soundboard 0.1.0
+Karl Skomski <karl@skomski.com>
+play sounds over your microphone
+
+USAGE:
+    soundboard.exe [FLAGS] [OPTIONS]
+
+FLAGS:
+    -h, --help                      Prints help information
+        --no-gui                    Disable GUI
+        --print-possible-devices    Print possible devices
+    -V, --version                   Prints version information
+
+OPTIONS:
+    -c, --config <FILE>                    sets a custom config file [default: soundboard.toml]
+    -i, --input-device <input-device>      Sets the input device to use
+    -o, --output-device <output-device>    Sets the output device to use
+        --verbose <verbose>                Sets the level of verbosity
+
+PS K:\projects\soundboard> .\target\debug\soundboard.exe --print-possible-devices
+  Devices:
+  0. "CABLE Input (VB-Audio Virtual Cable)"
+    Default output stream format:
+      Format { channels: 2, sample_rate: SampleRate(48000), data_type: F32 }
+  1. "Digital Audio (S/PDIF) (High Definition Audio Device)"
+    Default output stream format:
+      Format { channels: 2, sample_rate: SampleRate(48000), data_type: F32 }
+  2. "Desktop Microphone (RØDE NT-USB Mini)"
+    Default input stream format:
+      Format { channels: 1, sample_rate: SampleRate(48000), data_type: F32 }
+  3. "CABLE Output (VB-Audio Virtual Cable)"
+    Default input stream format:
+      Format { channels: 2, sample_rate: SampleRate(44100), data_type: F32 }
+  4. "Microphone (HD Webcam C525)"
+    Default input stream format:
+      Format { channels: 1, sample_rate: SampleRate(48000), data_type: F32 }
+
+PS K:\projects\soundboard> .\target\debug\soundboard.exe -i 2 -o 0 --no-gui
+  Using Devices:
+  2. "Desktop Microphone (RØDE NT-USB Mini)"
+    Default input stream format:
+      Format { channels: 1, sample_rate: SampleRate(48000), data_type: F32 }
+  0. "CABLE Input (VB-Audio Virtual Cable)"
+    Default output stream format:
+      Format { channels: 2, sample_rate: SampleRate(48000), data_type: F32 }
+Attempting to build input stream with `Format { channels: 1, sample_rate: SampleRate(48000), data_type: F32 }`.
+Successfully built input stream.
+Playing sound: K:\projects\soundboard\resources/nicht-so-tief-rudiger.mp3
+
+Press CTRL-C to exit
+````
---
+++ src/main.rs
@@ -0,0 +1,258 @@
+use clap::{crate_authors, crate_version, App, Arg};
+use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
+use hotkey;
+use iced::{
+    button, executor, Align, Application, Button, Column, Command, Element, Settings, Subscription,
+    Text,
+};
+use rodio;
+use std::path::PathBuf;
+use std::io::BufReader;
+
+fn print_possible_devices() {
+    let host = cpal::default_host();
+
+    let devices = host.devices().expect("No available sound devices");
+
+    println!("  Devices: ");
+    for (device_index, device) in devices.enumerate() {
+        println!("  {}. \"{}\"", device_index, device.name().unwrap());
+
+        // Input configs
+        if let Ok(conf) = device.default_input_format() {
+            println!("    Default input stream format:\n      {:?}", conf);
+        }
+
+        // Output configs
+        if let Ok(conf) = device.default_output_format() {
+            println!("    Default output stream format:\n      {:?}", conf);
+        }
+    }
+}
+
+fn sound_thread(input_device_index: usize, output_device_index: usize) {
+    let host = cpal::default_host();
+
+    let devices: Vec<_> = host
+        .devices()
+        .expect("No available sound devices")
+        .collect();
+    let input_device = devices
+        .get(input_device_index)
+        .expect("invalid input device specified");
+    let output_device = devices
+        .get(output_device_index)
+        .expect("invalid output device specified");
+    println!("  Using Devices: ");
+    println!(
+        "  {}. \"{}\"",
+        input_device_index,
+        input_device.name().unwrap()
+    );
+    // Input configs
+    if let Ok(conf) = input_device.default_input_format() {
+        println!("    Default input stream format:\n      {:?}", conf);
+    }
+
+    println!(
+        "  {}. \"{}\"",
+        output_device_index,
+        output_device.name().unwrap()
+    );
+    // Output configs
+    if let Ok(conf) = output_device.default_output_format() {
+        println!("    Default output stream format:\n      {:?}", conf);
+    }
+
+    let sink = rodio::Sink::new(&output_device);
+
+    std::thread::spawn(move || {
+        let mut hk = hotkey::Listener::new();
+        hk.register_hotkey(hotkey::modifiers::CONTROL, 'P' as u32, move || {
+            let mut file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            file_path.push("resources/nicht-so-tief-rudiger.mp3");
+            let file_path_string = file_path.to_str().unwrap();
+            let file = std::fs::File::open(&file_path).unwrap();
+            sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+            println!("Playing sound: {}", file_path_string);
+        })
+        .unwrap();
+
+        hk.listen();
+    });
+
+    let sink2 = rodio::Sink::new(&output_device);
+
+    let host = cpal::default_host();
+    let event_loop = host.event_loop();
+
+    let input_format = input_device.default_input_format().unwrap();
+
+    // // Build streams.
+    println!(
+        "Attempting to build input stream with `{:?}`.",
+        input_format
+    );
+    let input_stream_id = event_loop
+        .build_input_stream(&input_device, &input_format)
+        .unwrap();
+    println!("Successfully built input stream.");
+
+    event_loop
+        .play_stream(input_stream_id.clone())
+        .expect("fail stream1");
+
+    event_loop.run(move |id, result| {
+        let data = match result {
+            Ok(data) => data,
+            Err(err) => {
+                eprintln!("an error occurred on stream {:?}: {}", id, err);
+                return;
+            }
+        };
+
+        match data {
+            cpal::StreamData::Input {
+                buffer: cpal::UnknownTypeInputBuffer::F32(buffer),
+            } => {
+                let mut new_buffer = Vec::new();
+                for sample in buffer.iter() {
+                    let sample = cpal::Sample::to_f32(sample);
+                    new_buffer.push(sample);
+                }
+                let buffer = rodio::buffer::SamplesBuffer::new(
+                    input_format.channels,
+                    input_format.sample_rate.0,
+                    new_buffer,
+                );
+                sink2.append(buffer);
+            }
+            _ => panic!("we're expecting f32 data"),
+        }
+    });
+}
+
+pub fn main() {
+    let matches = App::new("soundboard")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("play sounds over your microphone")
+        .arg(
+            Arg::with_name("config-file")
+                .short('c')
+                .long("config")
+                .value_name("FILE")
+                .default_value("soundboard.toml")
+                .about("sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("input-device")
+                .short('i')
+                .long("input-device")
+                .about("Sets the input device to use")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("output-device")
+                .short('o')
+                .long("output-device")
+                .about("Sets the output device to use")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .long("verbose")
+                .takes_value(true)
+                .about("Sets the level of verbosity"),
+        )
+        .arg(
+            Arg::with_name("print-possible-devices")
+                .long("print-possible-devices")
+                .about("Print possible devices"),
+        )
+        .arg(Arg::with_name("no-gui").long("no-gui").about("Disable GUI"))
+        .get_matches();
+
+    if matches.is_present("print-possible-devices") {
+        print_possible_devices();
+        return;
+    }
+
+    let input_device_index: usize = matches
+        .value_of("input-device")
+        .expect("No input device specified")
+        .parse()
+        .expect("No number specified");
+    let output_device_index: usize = matches
+        .value_of("output-device")
+        .expect("No ouput device specified")
+        .parse()
+        .expect("No number specified");
+
+    let handle = std::thread::spawn(move || {
+        sound_thread(input_device_index, output_device_index);
+    });
+
+    if matches.is_present("no-gui") {
+        handle.join().expect("sound_thread join failed");
+        return;
+    }
+
+    let mut settings = Settings::default();
+    settings.window.size = (275, 150);
+    Soundboard::run(settings);
+}
+
+#[derive(Default)]
+struct Soundboard {
+    play_sound_button: button::State,
+    status_text: String,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    PlaySound,
+}
+
+impl Application for Soundboard {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Soundboard, Command<Message>) {
+        (Self::default(), Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("soundboard")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::PlaySound => {
+                self.status_text = "Start playing sound...".to_string();
+            }
+        }
+
+        Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        Subscription::none()
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .push(Text::new(self.title()).size(32))
+            .push(
+                Button::new(&mut self.play_sound_button, Text::new("Play sound"))
+                    .on_press(Message::PlaySound),
+            )
+            .padding(10)
+            .push(Text::new(&self.status_text).size(10))
+            .into()
+    }
+}
