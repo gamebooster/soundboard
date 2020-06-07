eab327e1912e075c86b45a1aef6e72534344b81a
Karl Skomski <karl@skomski.com>
2020-06-07 14:55:46 +0200

    gui: seperate panel_view

--- src/gui.rs
+++ src/gui.rs
@@ -4,108 +4,47 @@ use iced::{
   Space, Subscription, Text, VerticalAlignment,
 };
 
-use random_color::{Color, Luminosity, RandomColor};
-
 use log::{error, info, trace, warn};
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::{Receiver, Sender};
 
-mod style;
 use super::config;
 use super::sound;
 use std::fmt;
+mod panel_view;
+mod style;
 
 pub struct Soundboard {
   config: config::Config,
-  sender: Sender<sound::Message>,
-  panes: pane_grid::State<Content>,
+  panel_view: panel_view::PanelView,
+  sound_sender: Sender<sound::Message>,
   stop_button_state: button::State,
   volume_slider_state: slider::State,
   current_volume: f32,
 }
 
 #[derive(Debug, Clone)]
-pub enum Message {
+pub enum SoundboardMessage {
   PlaySound(String),
   StopAllSound,
-  Dragged(pane_grid::DragEvent),
-  Resized(pane_grid::ResizeEvent),
   VolumeChanged(f32),
+  HandlePanelViewMessage(panel_view::PanelViewMessage),
 }
 
 impl Application for Soundboard {
   type Executor = executor::Default;
-  type Message = Message;
+  type Message = SoundboardMessage;
   type Flags = (Sender<sound::Message>, config::Config);
 
-  fn new(flags: Self::Flags) -> (Soundboard, Command<Message>) {
-    let (panes, first) = pane_grid::State::<Content>::new(Content::new(SoundButton::default()));
-    let mut soundboard = Soundboard {
-      config: flags.1,
-      sender: flags.0,
-      panes: panes,
+  fn new(flags: Self::Flags) -> (Soundboard, Command<SoundboardMessage>) {
+    let soundboard = Soundboard {
+      config: flags.1.clone(),
+      sound_sender: flags.0,
       stop_button_state: button::State::new(),
       volume_slider_state: slider::State::new(),
       current_volume: 1.0,
+      panel_view: panel_view::PanelView::new(&flags.1.clone().sounds.unwrap()),
     };
-    let sounds = &soundboard.config.clone().sounds.unwrap();
-    let mut panels: Vec<pane_grid::Pane> = Vec::new();
-    panels.push(first);
-    sounds.iter().for_each(|sound| {
-      let modifier_string = sound
-        .hotkey_modifier
-        .clone()
-        .unwrap_or_default()
-        .into_iter()
-        .fold(String::new(), |all, one| {
-          if all.len() > 0 {
-            format!("{}-{}", all, one)
-          } else {
-            one.to_string()
-          }
-        });
-      let hotkey_string = {
-        if sound.hotkey_key.is_some() {
-          if modifier_string.len() > 0 {
-            format!(
-              "{}-{}",
-              modifier_string,
-              sound.hotkey_key.unwrap().to_string()
-            )
-          } else {
-            sound.hotkey_key.unwrap().to_string()
-          }
-        } else {
-          String::new()
-        }
-      };
-
-      let power = 2;
-      let power2_less = (panels.len() as f64).log(power as f64) as usize;
-      let index = { panels.len() - (power as usize).pow(power2_less as u32) };
-      let best_axis = {
-        if power2_less % power == 0 {
-          pane_grid::Axis::Horizontal
-        } else {
-          pane_grid::Axis::Vertical
-        }
-      };
-      let new_panel = soundboard
-        .panes
-        .split(
-          best_axis,
-          &panels[index],
-          Content::new(SoundButton {
-            state: button::State::new(),
-            path: sound.path.clone(),
-            name: sound.name.clone(),
-            hotkey: hotkey_string,
-          }),
-        )
-        .unwrap();
-      panels.push(new_panel);
-    });
-    soundboard.panes.close(&first);
     (soundboard, Command::none())
   }
 
@@ -113,58 +52,47 @@ impl Application for Soundboard {
     String::from("soundboard")
   }
 
-  fn update(&mut self, message: Message) -> Command<Message> {
+  fn update(&mut self, message: SoundboardMessage) -> Command<SoundboardMessage> {
     match message {
-      Message::PlaySound(sound_path) => {
-        if let Err(err) = self.sender.send(sound::Message::PlaySound(
+      SoundboardMessage::PlaySound(sound_path) => {
+        if let Err(err) = self.sound_sender.send(sound::Message::PlaySound(
           sound_path.clone(),
           sound::SoundDevices::Both,
         )) {
           error!("failed to play sound {}", err);
         };
       }
-      Message::StopAllSound => {
-        if let Err(err) = self.sender.send(sound::Message::StopAll) {
+      SoundboardMessage::StopAllSound => {
+        if let Err(err) = self.sound_sender.send(sound::Message::StopAll) {
           error!("failed to stop all sound {}", err);
         };
       }
-      Message::VolumeChanged(new_volume) => {
+      SoundboardMessage::VolumeChanged(new_volume) => {
         self.current_volume = new_volume;
         if let Err(err) = self
-          .sender
+          .sound_sender
           .send(sound::Message::SetVolume(self.current_volume))
         {
           error!("failed to set volume {}", err);
         };
       }
-      Message::Resized(pane_grid::ResizeEvent { split, ratio }) => {
-        self.panes.resize(&split, ratio);
+      SoundboardMessage::HandlePanelViewMessage(panel_view_message) => {
+        if let panel_view::PanelViewMessage::PlaySound(path) = panel_view_message {
+          self.update(SoundboardMessage::PlaySound(path));
+        } else {
+          self.panel_view.update(panel_view_message);
+        }
       }
-      Message::Dragged(pane_grid::DragEvent::Dropped { pane, target }) => {
-        self.panes.swap(&pane, &target);
-      }
-      Message::Dragged(_) => {}
     }
 
     Command::none()
   }
 
-  fn subscription(&self) -> Subscription<Message> {
+  fn subscription(&self) -> Subscription<SoundboardMessage> {
     Subscription::none()
   }
 
-  fn view(&mut self) -> Element<Message> {
-    let total_panes = self.panes.len();
-
-    let pane_grid = PaneGrid::new(&mut self.panes, |pane, content, focus| {
-      content.view(pane, focus, total_panes)
-    })
-    .width(Length::Fill)
-    .height(Length::FillPortion(18))
-    .on_drag(Message::Dragged)
-    .on_resize(Message::Resized)
-    .spacing(10);
-
+  fn view(&mut self) -> Element<SoundboardMessage> {
     let stop_button_column = Column::new()
       .spacing(5)
       .align_items(Align::Center)
@@ -187,7 +115,7 @@ impl Application for Soundboard {
       .height(Length::FillPortion(2))
       .push(
         Button::new(&mut self.stop_button_state, stop_button_container)
-          .on_press(Message::StopAllSound)
+          .on_press(SoundboardMessage::StopAllSound)
           .height(Length::Fill)
           .width(Length::FillPortion(2))
           .style(style::Button::Destructive),
@@ -202,7 +130,7 @@ impl Application for Soundboard {
           &mut self.volume_slider_state,
           0.0..=1.0,
           self.current_volume,
-          Message::VolumeChanged,
+          SoundboardMessage::VolumeChanged,
         )
         .width(Length::FillPortion(3)),
       );
@@ -211,7 +139,12 @@ impl Application for Soundboard {
       .width(Length::Fill)
       .height(Length::Fill)
       .spacing(10)
-      .push(pane_grid)
+      .push(
+        self
+          .panel_view
+          .view()
+          .map(move |message| SoundboardMessage::HandlePanelViewMessage(message)),
+      )
       .push(Space::with_height(Length::Units(5)))
       .push(bottom_row);
 
@@ -219,85 +152,6 @@ impl Application for Soundboard {
       .width(Length::Fill)
       .height(Length::Fill)
       .padding(10)
-      .into()
-  }
-}
-
-#[derive(Debug, Clone, Default)]
-struct SoundButton {
-  state: button::State,
-  name: String,
-  path: String,
-  hotkey: String,
-}
-
-struct Content {
-  //scroll: scrollable::State,
-  sound_button: SoundButton,
-  background_color: iced::Color,
-}
-
-impl Content {
-  fn new(sound_button: SoundButton) -> Self {
-    //let random_color= RandomColor::new()
-    //.luminosity(Luminosity::Light).to_rgb_array();
-    Content {
-      //scroll: scrollable::State::new(),
-      sound_button: sound_button,
-      background_color: iced::Color::from_rgb(0.2, 0.8, 0.2),
-      //iced::Color::from_rgb((random_color[0] as f32) / 255.0, (random_color[1] as f32) / 255.0, (random_color[2] as f32) / 255.0)
-    }
-  }
-  fn view(
-    &mut self,
-    _pane: pane_grid::Pane,
-    _focus: Option<pane_grid::Focus>,
-    _total_panes: usize,
-  ) -> Element<Message> {
-    let Content {
-      //scroll: _,
-      sound_button,
-      background_color,
-    } = self;
-
-    // let content = Scrollable::new(scroll)
-    //     .width(Length::Fill)
-    //     .spacing(10)
-    //     .align_items(Align::Center)
-    //     .push(Text::new("Pane").size(30));
-
-    let column = Column::new()
-      .spacing(5)
-      .align_items(Align::Center)
-      .width(Length::Fill)
-      .push(
-        Text::new(sound_button.name.clone())
-          .size(18)
-          .vertical_alignment(VerticalAlignment::Center),
-      )
-      .push(
-        Text::new(sound_button.hotkey.clone())
-          .size(14)
-          .vertical_alignment(VerticalAlignment::Center),
-      );
-
-    let cont = Container::new(column)
-      .width(Length::Fill)
-      .height(Length::Fill)
-      .padding(3)
-      .center_y();
-
-    let button = Button::new(&mut sound_button.state, cont)
-      .on_press(Message::PlaySound(sound_button.path.clone()))
-      .width(Length::Fill)
-      .height(Length::Fill)
-      .style(style::Button::Constructive(*background_color));
-
-    Container::new(button)
-      .width(Length::Fill)
-      .height(Length::Fill)
-      .padding(3)
-      .center_y()
       .into()
   }
 }
---
+++ src/gui/panel_view.rs
@@ -0,0 +1,184 @@
+use iced::{
+  button, executor, keyboard, pane_grid, scrollable, slider, Align, Application, Button, Column,
+  Command, Container, Element, Length, PaneGrid, ProgressBar, Row, Scrollable, Settings, Slider,
+  Space, Subscription, Text, VerticalAlignment,
+};
+
+use super::config;
+use super::style;
+use log::{error, info, trace, warn};
+
+#[derive(Debug, Clone, Default)]
+struct SoundButton {
+  state: button::State,
+  name: String,
+  path: String,
+  hotkey: String,
+}
+
+pub struct PanelView {
+  panes: pane_grid::State<PanelButtonView>,
+}
+
+#[derive(Debug, Clone)]
+pub enum PanelViewMessage {
+  Dragged(pane_grid::DragEvent),
+  Resized(pane_grid::ResizeEvent),
+  PlaySound(String),
+}
+
+impl PanelView {
+  pub fn new(sounds: &Vec<config::SoundConfig>) -> Self {
+    let (mut pane_state, first) =
+      pane_grid::State::<PanelButtonView>::new(PanelButtonView::new(SoundButton::default()));
+    let mut panels: Vec<pane_grid::Pane> = Vec::new();
+    panels.push(first);
+    sounds.iter().for_each(|sound| {
+      let modifier_string = sound
+        .hotkey_modifier
+        .clone()
+        .unwrap_or_default()
+        .into_iter()
+        .fold(String::new(), |all, one| {
+          if all.len() > 0 {
+            format!("{}-{}", all, one)
+          } else {
+            one.to_string()
+          }
+        });
+      let hotkey_string = {
+        if sound.hotkey_key.is_some() {
+          if modifier_string.len() > 0 {
+            format!(
+              "{}-{}",
+              modifier_string,
+              sound.hotkey_key.unwrap().to_string()
+            )
+          } else {
+            sound.hotkey_key.unwrap().to_string()
+          }
+        } else {
+          String::new()
+        }
+      };
+
+      let power = 2;
+      let power2_less = (panels.len() as f64).log(power as f64) as usize;
+      let index = { panels.len() - (power as usize).pow(power2_less as u32) };
+      let best_axis = {
+        if power2_less % power == 0 {
+          pane_grid::Axis::Horizontal
+        } else {
+          pane_grid::Axis::Vertical
+        }
+      };
+      let new_panel = pane_state
+        .split(
+          best_axis,
+          &panels[index],
+          PanelButtonView::new(SoundButton {
+            state: button::State::new(),
+            path: sound.path.clone(),
+            name: sound.name.clone(),
+            hotkey: hotkey_string,
+          }),
+        )
+        .unwrap();
+      panels.push(new_panel);
+    });
+    pane_state.close(&first);
+    return PanelView { panes: pane_state };
+  }
+
+  pub fn update(&mut self, message: PanelViewMessage) -> Command<PanelViewMessage> {
+    match message {
+      PanelViewMessage::Resized(pane_grid::ResizeEvent { split, ratio }) => {
+        self.panes.resize(&split, ratio);
+      }
+      PanelViewMessage::Dragged(pane_grid::DragEvent::Dropped { pane, target }) => {
+        self.panes.swap(&pane, &target);
+      }
+      PanelViewMessage::Dragged(_) => {}
+      PanelViewMessage::PlaySound(_) => {
+        unimplemented!();
+      }
+    }
+    Command::none()
+  }
+
+  pub fn view(&mut self) -> Element<PanelViewMessage> {
+    PaneGrid::new(&mut self.panes, |pane, content, focus| {
+      content.view(pane, focus)
+    })
+    .width(Length::Fill)
+    .height(Length::FillPortion(18))
+    .on_drag(PanelViewMessage::Dragged)
+    .on_resize(PanelViewMessage::Resized)
+    .spacing(10)
+    .into()
+  }
+}
+
+struct PanelButtonView {
+  //scroll: scrollable::State,
+  sound_button: SoundButton,
+  background_color: iced::Color,
+}
+
+impl PanelButtonView {
+  fn new(sound_button: SoundButton) -> Self {
+    //let random_color= RandomColor::new()
+    //.luminosity(Luminosity::Light).to_rgb_array();
+    PanelButtonView {
+      //scroll: scrollable::State::new(),
+      sound_button: sound_button,
+      background_color: iced::Color::from_rgb(0.2, 0.8, 0.2),
+      //iced::Color::from_rgb((random_color[0] as f32) / 255.0, (random_color[1] as f32) / 255.0, (random_color[2] as f32) / 255.0)
+    }
+  }
+  fn view(
+    &mut self,
+    _pane: pane_grid::Pane,
+    _focus: Option<pane_grid::Focus>,
+  ) -> Element<PanelViewMessage> {
+    let PanelButtonView {
+      //scroll: _,
+      sound_button,
+      background_color,
+    } = self;
+
+    // let content = Scrollable::new(scroll)
+    //     .width(Length::Fill)
+    //     .spacing(10)
+    //     .align_items(Align::Center)
+    //     .push(Text::new("Pane").size(30));
+
+    let column = Column::new()
+      .spacing(5)
+      .align_items(Align::Center)
+      .width(Length::Fill)
+      .push(
+        Text::new(sound_button.name.clone())
+          .size(18)
+          .vertical_alignment(VerticalAlignment::Center),
+      )
+      .push(
+        Text::new(sound_button.hotkey.clone())
+          .size(14)
+          .vertical_alignment(VerticalAlignment::Center),
+      );
+
+    let cont = Container::new(column)
+      .width(Length::Fill)
+      .height(Length::Fill)
+      .padding(3)
+      .center_y();
+
+    Button::new(&mut sound_button.state, cont)
+      .on_press(PanelViewMessage::PlaySound(sound_button.path.clone()))
+      .width(Length::Fill)
+      .height(Length::Fill)
+      .style(style::Button::Constructive(*background_color))
+      .into()
+  }
+}
