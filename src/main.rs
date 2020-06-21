c965a43543c52d882e719444ab9d5f893e2f6d56
Karl Skomski <karl@skomski.com>
2020-06-21 12:30:53 +0200

    telegram: add play commands

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate strum_macros;
 
 use anyhow::{anyhow, Context, Result};
 use log::{error, info, trace, warn};
+use std::panic;
 
 #[cfg(feature = "gui")]
 extern crate iced;
@@ -29,166 +30,195 @@ mod sound;
 mod utils;
 
 fn main() {
-  if let Err(err) = try_main() {
-    error!(
+    macro_rules! FATAL_ERROR_MESSAGE {
+    () => {
       r"
-    soundboard encountered an fatal error:
-        Please file a bug report if unexpected at https://github.com/gamebooster/soundboard/issues
-    Description:
-        {:?}",
-      err
-    );
-    std::process::exit(1);
-  }
-  info!("Auf Wiedersehen!");
+soundboard encountered an fatal error:
+    Please file a bug report if unexpected at https://github.com/gamebooster/soundboard/issues
+Description:
+    {:?}
+Location:
+    {}"
+    };
+  };
+    panic::set_hook(Box::new(|panic_info| {
+        let mut location_info = String::new();
+        if let Some(location) = panic_info.location() {
+            location_info += &format!(
+                "panic occurred in file '{}' at line {}",
+                location.file(),
+                location.line(),
+            );
+        } else {
+            location_info += "panic occurred but can't get location information...";
+        }
+        if let Some(payload) = panic_info.payload().downcast_ref::<&str>() {
+            error!(FATAL_ERROR_MESSAGE!(), payload, location_info);
+        } else if let Some(payload) = panic_info.payload().downcast_ref::<String>() {
+            error!(FATAL_ERROR_MESSAGE!(), payload, location_info);
+        } else {
+            error!(
+                FATAL_ERROR_MESSAGE!(),
+                "No description location: {}", location_info
+            );
+        }
+
+        std::process::exit(1);
+    }));
+
+    if let Err(err) = try_main() {
+        error!(FATAL_ERROR_MESSAGE!(), err, "No location");
+        std::process::exit(1);
+    }
+    info!("Auf Wiedersehen!");
 }
 
 fn try_main() -> Result<()> {
-  env_logger::builder()
-    .filter_module("soundboard", log::LevelFilter::Trace)
-    .filter_module("warp", log::LevelFilter::Info)
-    .init();
-  info!("Parsing arguments");
-  let arguments = config::parse_arguments();
+    env_logger::builder()
+        .filter_module("soundboard", log::LevelFilter::Trace)
+        .filter_module("warp", log::LevelFilter::Info)
+        .init();
+    info!("Parsing arguments");
+    let arguments = config::parse_arguments();
 
-  if arguments.is_present("print-possible-devices") {
-    sound::print_possible_devices(true);
-    return Ok(());
-  }
-
-  let config_file_name = arguments.value_of("config-file").unwrap().to_string();
-  let config_file = config::load_and_parse_config(&config_file_name)?;
-
-  let (sound_sender, gui_receiver): (
-    crossbeam_channel::Sender<sound::Message>,
-    crossbeam_channel::Receiver<sound::Message>,
-  ) = crossbeam_channel::unbounded();
-
-  let (gui_sender, sound_receiver): (
-    crossbeam_channel::Sender<sound::Message>,
-    crossbeam_channel::Receiver<sound::Message>,
-  ) = crossbeam_channel::unbounded();
-
-  let (input_device_index, output_device_index, loop_device_index) =
-    config::parse_devices(&config_file, &arguments)?;
-
-  let sound_receiver_clone = sound_receiver;
-  let sound_sender_clone = sound_sender;
-  let _sound_thread_handle = std::thread::spawn(move || {
-    if let Err(err) = sound::init_sound(
-      sound_receiver_clone,
-      sound_sender_clone,
-      input_device_index,
-      output_device_index,
-      loop_device_index,
-    ) {
-      error!("init sound thread error:\n\t {}", err);
+    if arguments.is_present("print-possible-devices") {
+        sound::print_possible_devices(true);
+        return Ok(());
     }
-  });
 
-  // test for sound thread successfull initialization
-  if let Err(err) = gui_sender.send(sound::Message::PlayStatus(Vec::new(), 0.0)) {
-    return Err(anyhow!(err));
-  }
-  if let Err(err) = gui_receiver.recv() {
-    return Err(anyhow!(err));
-  }
+    let config_file_name = arguments.value_of("config-file").unwrap().to_string();
+    let config_file = config::load_and_parse_config(&config_file_name)?;
 
-  #[cfg(feature = "http")]
-  {
-    if arguments.is_present("http-server")
-      || config_file.http_server.unwrap_or_default()
-      || std::env::var("SB_HTTPSERVER").is_ok()
+    let (sound_sender, gui_receiver): (
+        crossbeam_channel::Sender<sound::Message>,
+        crossbeam_channel::Receiver<sound::Message>,
+    ) = crossbeam_channel::unbounded();
+
+    let (gui_sender, sound_receiver): (
+        crossbeam_channel::Sender<sound::Message>,
+        crossbeam_channel::Receiver<sound::Message>,
+    ) = crossbeam_channel::unbounded();
+
+    let (input_device_index, output_device_index, loop_device_index) =
+        config::parse_devices(&config_file, &arguments)?;
+
+    let sound_receiver_clone = sound_receiver;
+    let sound_sender_clone = sound_sender;
+    let _sound_thread_handle = std::thread::spawn(move || {
+        if let Err(err) = sound::init_sound(
+            sound_receiver_clone,
+            sound_sender_clone,
+            input_device_index,
+            output_device_index,
+            loop_device_index,
+        ) {
+            error!("init sound thread error:\n\t {}", err);
+        }
+    });
+
+    // test for sound thread successfull initialization
+    if let Err(err) = gui_sender.send(sound::Message::PlayStatus(Vec::new(), 0.0)) {
+        return Err(anyhow!(err));
+    }
+    if let Err(err) = gui_receiver.recv() {
+        return Err(anyhow!(err));
+    }
+
+    #[cfg(feature = "http")]
     {
-      let gui_sender_clone = gui_sender.clone();
-      let gui_receiver_clone = gui_receiver.clone();
-      let config_file_name = config_file_name.clone();
-      std::thread::spawn(move || {
-        http_server::run(config_file_name, gui_sender_clone, gui_receiver_clone);
-      });
+        if arguments.is_present("http-server")
+            || config_file.http_server.unwrap_or_default()
+            || std::env::var("SB_HTTPSERVER").is_ok()
+        {
+            let gui_sender_clone = gui_sender.clone();
+            let gui_receiver_clone = gui_receiver.clone();
+            let config_file_name = config_file_name.clone();
+            std::thread::spawn(move || {
+                http_server::run(config_file_name, gui_sender_clone, gui_receiver_clone);
+            });
+        }
     }
-  }
 
-  #[cfg(feature = "telegram")]
-  {
-    if arguments.is_present("telegram") || std::env::var("SB_TELEGRAM").is_ok() {
-      let gui_sender_clone = gui_sender.clone();
-      let gui_receiver_clone = gui_receiver.clone();
-      let config_file_name = config_file_name.clone();
-      std::thread::spawn(move || {
-        telegram::run(config_file_name, gui_sender_clone, gui_receiver_clone);
-      });
-    }
-  }
-
-  #[cfg(feature = "gui")]
-  {
-    if arguments.is_present("no-gui")
-      || config_file.no_gui.unwrap_or_default()
-      || std::env::var("SB_NOGUI").is_ok()
+    #[cfg(feature = "telegram")]
     {
-      no_gui_routine(config_file, gui_sender)?;
-      std::thread::park();
-      return Ok(());
+        if arguments.is_present("telegram") || std::env::var("SB_TELEGRAM").is_ok() {
+            let gui_sender_clone = gui_sender.clone();
+            let gui_receiver_clone = gui_receiver.clone();
+            let config_file_name = config_file_name.clone();
+            std::thread::spawn(move || {
+                telegram::run(config_file_name, gui_sender_clone, gui_receiver_clone);
+            });
+        }
     }
-    let mut settings = Settings::with_flags((gui_sender, gui_receiver, config_file_name));
-    settings.window.size = (500, 350);
-    gui::Soundboard::run(settings);
-  }
-  #[cfg(not(feature = "gui"))]
-  {
-    let config_file_clone = config_file.clone();
-    no_gui_routine(config_file_clone, gui_sender)?;
-  }
-  Ok(())
+
+    #[cfg(feature = "gui")]
+    {
+        if arguments.is_present("no-gui")
+            || config_file.no_gui.unwrap_or_default()
+            || std::env::var("SB_NOGUI").is_ok()
+        {
+            no_gui_routine(config_file, gui_sender)?;
+            std::thread::park();
+            return Ok(());
+        }
+        let mut settings = Settings::with_flags((gui_sender, gui_receiver, config_file_name));
+        settings.window.size = (500, 350);
+        gui::Soundboard::run(settings);
+    }
+    #[cfg(not(feature = "gui"))]
+    {
+        let config_file_clone = config_file.clone();
+        no_gui_routine(config_file_clone, gui_sender)?;
+    }
+    Ok(())
 }
 
 fn no_gui_routine(
-  config_file: config::MainConfig,
-  gui_sender: crossbeam_channel::Sender<sound::Message>,
+    config_file: config::MainConfig,
+    gui_sender: crossbeam_channel::Sender<sound::Message>,
 ) -> Result<()> {
-  let mut hotkey_manager = hotkey::HotkeyManager::new();
+    let mut hotkey_manager = hotkey::HotkeyManager::new();
 
-  let stop_hotkey = {
-    if config_file.stop_hotkey.is_some() {
-      config::parse_hotkey(&config_file.stop_hotkey.as_ref().unwrap())?
-    } else {
-      config::Hotkey {
-        modifier: vec![config::Modifier::CTRL],
-        key: config::Key::S,
-      }
+    let stop_hotkey = {
+        if config_file.stop_hotkey.is_some() {
+            config::parse_hotkey(&config_file.stop_hotkey.as_ref().unwrap())?
+        } else {
+            config::Hotkey {
+                modifier: vec![config::Modifier::CTRL],
+                key: config::Key::S,
+            }
+        }
+    };
+    let gui_sender_clone = gui_sender.clone();
+    hotkey_manager
+        .register(stop_hotkey, move || {
+            let _result = gui_sender_clone.send(sound::Message::StopAll);
+        })
+        .map_err(|_s| anyhow!("register key"))?;
+
+    let gui_sender_clone = gui_sender;
+    // only register hotkeys for first soundboard in no-gui-mode
+    for sound in config_file.soundboards[0]
+        .sounds
+        .clone()
+        .unwrap_or_default()
+    {
+        if sound.hotkey.is_none() {
+            continue;
+        }
+        let hotkey = config::parse_hotkey(&sound.hotkey.as_ref().unwrap())?;
+        let tx_clone = gui_sender_clone.clone();
+        info!("register hotkey  {} for sound {}", &hotkey, sound.name);
+        let _result = hotkey_manager.register(hotkey, move || {
+            if let Err(err) = tx_clone.send(sound::Message::PlaySound(
+                sound.clone(),
+                sound::SoundDevices::Both,
+            )) {
+                error!("failed to play sound {}", err);
+            };
+        })?;
     }
-  };
-  let gui_sender_clone = gui_sender.clone();
-  hotkey_manager
-    .register(stop_hotkey, move || {
-      let _result = gui_sender_clone.send(sound::Message::StopAll);
-    })
-    .map_err(|_s| anyhow!("register key"))?;
 
-  let gui_sender_clone = gui_sender;
-  // only register hotkeys for first soundboard in no-gui-mode
-  for sound in config_file.soundboards[0]
-    .sounds
-    .clone()
-    .unwrap_or_default()
-  {
-    if sound.hotkey.is_none() {
-      continue;
-    }
-    let hotkey = config::parse_hotkey(&sound.hotkey.as_ref().unwrap())?;
-    let tx_clone = gui_sender_clone.clone();
-    info!("register hotkey  {} for sound {}", &hotkey, sound.name);
-    let _result = hotkey_manager.register(hotkey, move || {
-      if let Err(err) = tx_clone.send(sound::Message::PlaySound(
-        sound.clone(),
-        sound::SoundDevices::Both,
-      )) {
-        error!("failed to play sound {}", err);
-      };
-    })?;
-  }
-
-  std::thread::park();
-  Ok(())
+    std::thread::park();
+    Ok(())
 }
--- src/telegram.rs
+++ src/telegram.rs
@@ -1,24 +1,46 @@
 #![allow(dead_code)]
-use super::config;
-use super::sound;
+use super::{config, sound};
 use anyhow::{anyhow, Result};
 use crossbeam_channel::{Receiver, Sender};
+use fuzzy_matcher::skim::SkimMatcherV2;
+use fuzzy_matcher::FuzzyMatcher;
 use log::{error, info, trace, warn};
-use std::env;
-use std::path::PathBuf;
-use tgbot::longpoll::LongPoll;
-use tgbot::methods::GetFile;
-use tgbot::methods::SendMessage;
-use tgbot::types::{Audio, Document, MessageData, Update, UpdateKind};
-use tgbot::{async_trait, Api, Config, UpdateHandler};
-use tokio;
-use tokio::stream::StreamExt;
-use tokio::{fs::File, io::AsyncWriteExt};
+use serde::{Deserialize, Serialize};
+use std::convert::TryFrom;
+use std::{env, path::PathBuf};
+use tgbot::{
+    async_trait,
+    longpoll::LongPoll,
+    methods::{GetFile, SendMessage},
+    types::{
+        Audio, CallbackQuery, Command, Document, InlineKeyboardButton, Message, MessageData,
+        Update, UpdateKind,
+    },
+    Api, Config, UpdateHandler,
+};
+use tokio::{self, fs::File, io::AsyncWriteExt, stream::StreamExt};
+
+type ConfigLockType = std::sync::Arc<std::sync::RwLock<config::MainConfig>>;
 
 struct Handler {
     api: Api,
     sender: Sender<sound::Message>,
     receiver: Receiver<sound::Message>,
+    config_file_name: String,
+    config: ConfigLockType,
+}
+
+#[derive(Deserialize, Serialize)]
+struct CallbackSoundSelectionData {
+    sound_name: String,
+}
+
+impl CallbackSoundSelectionData {
+    fn new<S: Into<String>>(value: S) -> Self {
+        Self {
+            sound_name: value.into(),
+        }
+    }
 }
 
 async fn download_file(api: &Api, file_id: &str, file_name: &str) -> Result<PathBuf> {
@@ -38,7 +60,21 @@ async fn download_file(api: &Api, file_id: &str, file_name: &str) -> Result<Path
     Ok(temp_path)
 }
 
-fn send_sound_config(sender: &Sender<sound::Message>, name: String, path: String) -> Result<()> {
+fn send_sound_config(
+    sender: &Sender<sound::Message>,
+    sound_config: config::SoundConfig,
+) -> Result<()> {
+    Ok(sender.send(sound::Message::PlaySound(
+        sound_config,
+        sound::SoundDevices::Both,
+    ))?)
+}
+
+fn send_new_sound_config(
+    sender: &Sender<sound::Message>,
+    name: String,
+    path: String,
+) -> Result<()> {
     Ok(sender.send(sound::Message::PlaySound(
         config::SoundConfig {
             name: name,
@@ -51,14 +87,14 @@ fn send_sound_config(sender: &Sender<sound::Message>, name: String, path: String
     ))?)
 }
 
-async fn handle_audio(api: &Api, sender: &Sender<sound::Message>, audio: Audio) -> Result<String> {
+async fn handle_audio(api: &Api, sender: &Sender<sound::Message>, audio: &Audio) -> Result<String> {
     let path = download_file(api, &audio.file_id, &audio.file_unique_id).await?;
 
     info!("Name: {:?}", audio.title);
     info!("Mime-Type: {:?}", audio.mime_type);
     info!("File size: {:?}", audio.file_size);
 
-    send_sound_config(
+    send_new_sound_config(
         sender,
         audio.title.clone().unwrap_or_default(),
         path.to_str().unwrap().to_string(),
@@ -70,7 +106,7 @@ async fn handle_audio(api: &Api, sender: &Sender<sound::Message>, audio: Audio) 
 async fn handle_document(
     api: &Api,
     sender: &Sender<sound::Message>,
-    document: Document,
+    document: &Document,
 ) -> Result<String> {
     let path = download_file(api, &document.file_id, &document.file_unique_id).await?;
 
@@ -78,7 +114,7 @@ async fn handle_document(
     info!("Mime-Type: {:?}", document.mime_type);
     info!("File size: {:?}", document.file_size);
 
-    send_sound_config(
+    send_new_sound_config(
         sender,
         document.file_name.clone().unwrap_or_default(),
         path.to_str().unwrap().to_string(),
@@ -87,60 +123,250 @@ async fn handle_document(
     return Ok(document.file_name.clone().unwrap_or_default());
 }
 
+async fn handle_stopall_command(
+    _api: &Api,
+    sender: &Sender<sound::Message>,
+    _config: ConfigLockType,
+    _message: &Message,
+    _raw_args: String,
+) {
+    sender
+        .send(sound::Message::StopAll)
+        .expect("sound channel error");
+}
+
+async fn handle_play_command(
+    api: &Api,
+    sender: &Sender<sound::Message>,
+    config: ConfigLockType,
+    message: &Message,
+    raw_args: String,
+) {
+    info!("handle_play_command arg: {}", raw_args);
+    if raw_args.len() == 0 {
+        let method = SendMessage::new(
+            message.get_chat_id(),
+            format!("You need to specify search string after /play!"),
+        );
+        api.execute(method).await.unwrap();
+        return;
+    }
+
+    let mut possible_matches: Vec<(i64, config::SoundConfig)> = Vec::new();
+    let matcher = SkimMatcherV2::default();
+    let max_matches = 8;
+
+    for soundboard in &config.read().unwrap().soundboards {
+        for sound in soundboard.sounds.as_ref().unwrap() {
+            if let Some(score) = matcher.fuzzy_match(&sound.name, &raw_args) {
+                if possible_matches.len() <= max_matches - 1 {
+                    possible_matches.push((score, sound.clone()));
+                    possible_matches.sort_unstable_by_key(|e| std::cmp::Reverse(e.0));
+                } else if possible_matches.last().unwrap().0 < score {
+                    possible_matches.push((score, sound.clone()));
+                    possible_matches.sort_unstable_by_key(|e| std::cmp::Reverse(e.0));
+                    possible_matches.pop();
+                }
+            }
+        }
+    }
+
+    if possible_matches.len() == 0 {
+        let method = SendMessage::new(
+            message.get_chat_id(),
+            format!("No matches for sound name: {}", raw_args),
+        );
+        api.execute(method).await.unwrap();
+    } else if possible_matches.len() == 1 {
+        send_sound_config(sender, possible_matches[0].1.clone()).expect("sound channel error");
+        let method = SendMessage::new(
+            message.get_chat_id(),
+            format!("Playing sound: {}", possible_matches[0].1.name),
+        );
+        api.execute(method).await.unwrap();
+    } else if possible_matches.len() <= max_matches {
+        let all_matches = possible_matches.iter().fold(Vec::new(), |mut acc, elem| {
+            acc.push(
+                InlineKeyboardButton::with_callback_data_struct(
+                    acc.len().to_string(),
+                    &CallbackSoundSelectionData::new(elem.1.name.clone()),
+                )
+                .unwrap(),
+            );
+            acc
+        });
+        let mut index = 0;
+        let all_matches_string = possible_matches.iter().fold(String::new(), |acc, elem| {
+            let res = format!("{} \n {}. {} ({})", acc, index, elem.1.name.clone(), elem.0);
+            index += 1;
+            res
+        });
+        let method = SendMessage::new(
+            message.get_chat_id(),
+            format!("Matches: \n {}", all_matches_string),
+        )
+        .reply_markup(vec![all_matches]);
+        api.execute(method).await.unwrap();
+    } else {
+        let method = SendMessage::new(
+            message.get_chat_id(),
+            format!("Over {} matches!", possible_matches.len()),
+        );
+        api.execute(method).await.unwrap();
+    }
+}
+
+fn play_sound_with_name(sender: &Sender<sound::Message>, config: ConfigLockType, name: &str) {
+    for soundboard in &config.read().unwrap().soundboards {
+        for sound in soundboard.sounds.as_ref().unwrap() {
+            if sound.name == name {
+                send_sound_config(sender, sound.clone()).expect("sound channel error");
+            }
+        }
+    }
+}
+
 #[async_trait]
 impl UpdateHandler for Handler {
     async fn handle(&mut self, update: Update) {
-        info!("got an update: {:?}\n", update);
+        // info!("got an update: {:?}\n", update);
 
-        if let UpdateKind::Message(message) = update.kind {
-            let chat_id = message.get_chat_id();
-            if let Some(text) = message.get_text() {
-                let api = self.api.clone();
-                let method = SendMessage::new(chat_id, text.data.clone());
-                api.execute(method).await.unwrap();
-            }
-            let api = self.api.clone();
-            let result;
-            match message.data {
-                MessageData::Audio { data, .. } => {
-                    result = handle_audio(&self.api, &self.sender, data).await;
-                }
-                MessageData::Document { data, .. } => {
-                    result = handle_document(&self.api, &self.sender, data).await;
-                }
-                _ => {
-                    return;
+        match update.kind {
+            UpdateKind::CallbackQuery(query) => {
+                if let Some(_) = query.message {
+                    let data = query
+                        .parse_data::<CallbackSoundSelectionData>()
+                        .unwrap()
+                        .unwrap();
+                    play_sound_with_name(
+                        &self.sender,
+                        self.config.clone(),
+                        &data.sound_name.clone(),
+                    );
+                    let method = tgbot::methods::AnswerCallbackQuery::new(query.id)
+                        .text(format!("Playing sound: {}", &data.sound_name));
+                    self.api.execute(method).await.unwrap();
                 }
             }
-            match result {
-                Ok(name) => {
-                    let method = SendMessage::new(chat_id, format!("PlaySound {}", name));
-                    api.execute(method).await.unwrap();
+            UpdateKind::Message(message) => {
+                if let Ok(command) = Command::try_from(message.clone()) {
+                    let pos = command.get_message().commands.as_ref().unwrap()[0]
+                        .data
+                        .offset
+                        + command.get_message().commands.as_ref().unwrap()[0]
+                            .data
+                            .length;
+                    let raw_args: Vec<u16> = message
+                        .get_text()
+                        .unwrap()
+                        .data
+                        .encode_utf16()
+                        .skip(pos)
+                        .collect();
+                    let raw_args = String::from_utf16(&raw_args).unwrap().trim().to_owned();
+
+                    match command.get_name() {
+                        "/play" => {
+                            handle_play_command(
+                                &self.api,
+                                &self.sender,
+                                self.config.clone(),
+                                command.get_message(),
+                                raw_args,
+                            )
+                            .await;
+                        }
+                        "/stopall" => {
+                            handle_stopall_command(
+                                &self.api,
+                                &self.sender,
+                                self.config.clone(),
+                                command.get_message(),
+                                raw_args,
+                            )
+                            .await;
+                        }
+                        _ => {
+                            let api = self.api.clone();
+                            let method = SendMessage::new(
+                                command.get_message().get_chat_id(),
+                                format!("Unsupported command received: {}", command.get_name()),
+                            );
+                            api.execute(method).await.unwrap();
+                        }
+                    }
+                } else {
+                    let result;
+                    match &message.data {
+                        MessageData::Audio { data, .. } => {
+                            result = handle_audio(&self.api, &self.sender, data).await;
+                        }
+                        MessageData::Document { data, .. } => {
+                            result = handle_document(&self.api, &self.sender, data).await;
+                        }
+                        _ => {
+                            return;
+                        }
+                    }
+
+                    let api = self.api.clone();
+
+                    match result {
+                        Ok(name) => {
+                            let method = SendMessage::new(
+                                message.get_chat_id(),
+                                format!("PlaySound {}", name),
+                            );
+                            api.execute(method).await.unwrap();
+                        }
+                        Err(err) => {
+                            let method = SendMessage::new(
+                                message.get_chat_id(),
+                                format!("PlaySoundError {}", err),
+                            );
+                            api.execute(method).await.unwrap();
+                        }
+                    }
                 }
-                Err(err) => {
-                    let method = SendMessage::new(chat_id, format!("PlaySoundError {}", err));
-                    api.execute(method).await.unwrap();
-                }
-            };
+            }
+            _ => {}
         }
     }
 }
 
 #[tokio::main]
 pub async fn run(
-    _config_file_name: String,
+    config_file_name: String,
     sender: Sender<sound::Message>,
     receiver: Receiver<sound::Message>,
 ) {
     let token = env::var("SB_TELEGRAM_TOKEN").expect("SB_TELEGRAM_TOKEN is not set");
     let api = Api::new(Config::new(token)).expect("Failed to create API");
+    api.execute(tgbot::methods::SetMyCommands::new(vec![
+        tgbot::types::BotCommand::new("play", "play the sound with the provided name (fuzzy)")
+            .unwrap(),
+        tgbot::types::BotCommand::new("stopall", "stop all sounds playing").unwrap(),
+    ]))
+    .await
+    .expect("SetMyCommands failed");
+
+    let config_file = config::load_and_parse_config(&config_file_name).unwrap();
+    let config = std::sync::Arc::new(std::sync::RwLock::new(config_file));
+
     LongPoll::new(
         api.clone(),
         Handler {
             api,
             sender,
             receiver,
+            config_file_name,
+            config,
         },
+    )
+    .options(
+        tgbot::longpoll::LongPollOptions::default()
+            .allowed_update(tgbot::types::AllowedUpdate::Message)
+            .allowed_update(tgbot::types::AllowedUpdate::CallbackQuery),
     )
     .run()
     .await;
