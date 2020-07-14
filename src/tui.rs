0f582b30351f3d5bb282e8e5f9703e063d143d89
Elena Frank <elena.frank@cgi.com>
2020-07-14 02:04:57 +0200

    first steps to actual play a sound from tui.rs

--- src/tui.rs
+++ src/tui.rs
@@ -15,19 +15,19 @@ use crossterm::{
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, ScrollUp, ScrollDown, SetSize, size, Clear, ClearType},
     cursor::{DisableBlinking, EnableBlinking, MoveTo, RestorePosition, SavePosition, Show, Hide}
 };
+use log::{error, info, trace, warn};
 
-use super::config::{SoundConfig, MainConfig};
+use super::config;
+use super::sound;
+use super::hotkey;
+
+use crossbeam_channel::unbounded;
 
 mod sound_state_list;
 
-
 pub fn draw_terminal() -> Result<()> {
-    execute!(stdout(), EnableMouseCapture)?;
-    execute!(stdout(), EnterAlternateScreen)?;
-    execute!(stdout(), Hide)?;
-    let backend = CrosstermBackend::new(stdout());
-    let mut terminal = Terminal::new(backend)?;
-    let current_sounds = MainConfig::read()
+    let mut hotkey_manager = hotkey::HotkeyManager::new();
+    let current_sounds = config::MainConfig::read()
         .soundboards
         .iter()
         .find(|s| s.name == "deutsche memes")
@@ -36,6 +36,31 @@ pub fn draw_terminal() -> Result<()> {
         .as_ref()
         .unwrap()
         .clone();
+    let (sound_sender, sound_receiver ) = unbounded();
+    for sound in &current_sounds {
+        if sound.hotkey.is_none() {
+            continue;
+        }
+        let hotkey = config::parse_hotkey(&sound.hotkey.as_ref().unwrap()).unwrap();
+        let sound = sound.clone();
+        let tx_clone = sound_sender.clone();
+        let _result = hotkey_manager.register(hotkey, move || {
+            if let Err(err) = tx_clone.send(sound::Message::PlaySound(
+                sound.clone(),
+                sound::SoundDevices::Both,
+            )) {
+                error!("failed to play sound {}", err);
+            };
+        });
+    }
+    if let Err(err) = sound_sender.send(sound::Message::SetVolume(1.0)){
+        error!("failed to set volume {}", err);
+    };
+    execute!(stdout(), EnableMouseCapture)?;
+    execute!(stdout(), EnterAlternateScreen)?;
+    execute!(stdout(), Hide)?;
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
     let mut sound_list = sound_state_list::SoundStateList::new(current_sounds);
     sound_list.state.select(Some(0));
     loop {
@@ -73,8 +98,14 @@ pub fn draw_terminal() -> Result<()> {
                     sound_list.previous();
                 }
                 KeyCode::Enter => {
-                    let selected = sound_list.state.selected().unwrap();
-                    println!("{:?}", selected);
+                    let selected_index = sound_list.state.selected().unwrap();
+                    let sound_config = sound_list.sounds[selected_index].clone();
+                    if let Err(err) = sound_sender.send(sound::Message::PlaySound(
+                        sound_config,
+                        sound::SoundDevices::Both,
+                    )) {
+                        error!("failed to play sound {}", err);
+                    };
 
                 }
                 _ => {}
